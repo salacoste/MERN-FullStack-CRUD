d399461ac9e7f7fd6daa72480cf957da24f94395
Ivan D <ivan.dergachev@list.ru>
2020-05-28 15:44:52 -0600

    finished RESTful API with Vue-based FE

--- app.js
+++ app.js
@@ -1,16 +1,42 @@
 const express = require('express')
 const path = require('path')
+const {v4} = require('uuid')
 
 const app = express()
 
-const CONTACTS = [{
-  id:1,
+let CONTACTS = [{
+  id: v4(),
   name: 'test',
   phone: '912929',
   marked: false
 }]
 
 app.use(express.static(path.resolve(__dirname)))
+
+app.use(express.json())
+
+app.post('/api/contacts',(req,res)=>{
+  const contact = {id:v4(), ...req.body, marked:false}
+  CONTACTS.push(contact)
+  res.status(201).json(contact)
+})
+
+app.delete('/api/contacts/:id', (req, res)=>{
+  CONTACTS = CONTACTS.filter(el=> el.id !== req.params.id)
+  res.status(200).json({message: "Contact is deleted", contacts: CONTACTS})
+})
+
+app.post('/api/contacts/:id', (req,res)=> {
+  console.log('req:', req)
+  const contact = CONTACTS.find(el=> el.id === req.body.id)
+  contact.marked = !contact.marked
+  res.status(200).json({message:`It's done`})
+})
+
+app.get('/api/contacts', (req, res) => {
+  res.status(200).json(CONTACTS)
+})
+
 
 app.get('/', (req,res) => {
   // console.log('req', req)
--- client/frontend.js
+++ client/frontend.js
@@ -1,5 +1,15 @@
 import Vue from 'https://cdn.jsdelivr.net/npm/vue@2.6.11/dist/vue.esm.browser.js'
 
+Vue.component('loader', {
+  template: `
+  <div >
+    <div class="spinner-border" role="status">
+      <span class="sr-only">Loading...</span>
+    </div>
+  </div>
+  `
+})
+
 new Vue({
   el: '#app',
   data() {
@@ -8,7 +18,8 @@ new Vue({
         name: '',
         phone: ''
       },
-      contacts: []
+      contacts: [],
+      loading: false,
     }
   },
   computed: {
@@ -17,18 +28,23 @@ new Vue({
     }
   },
   methods: {
-    createContact() {
+    async createContact() {
       const {...res} = this.form
 
       console.log(res);
-      this.contacts.push({...res, id: Date.now(), marked: false})
+      const contact = await request('/api/contacts', 'POST', res)
+      this.contacts.push(contact)
+      // this.contacts.push({...res, id: Date.now(), marked: false})
       
-      console.log(this.contacts);
+      
+      // console.log(this.contacts);
       
       this.form.name = this.form.phone = ''
       
     },
-    markContact(id) {
+    async markContact(id) {
+
+      await request(`/api/contacts/${id}`, 'POST', {id})
       console.log(`contact is marked ${id}`)
       this.contacts.forEach((el)=> {
         if (el.id === id) {
@@ -36,12 +52,51 @@ new Vue({
         }
       })
     },
-    deleteContact(id) {
-
-      console.log(`contact is deleted ${id}`);
+    async deleteContact(id) {
+      await request(`/api/contacts/${id}`, 'DELETE')
+      // await console.log(`contact is deleted ${id}`)
       this.contacts = this.contacts.filter((el)=> el.id !== id)
       console.log(this.contacts)
       
     }
+  },
+  mounted() {
+    this.loading = true
+    request('/api/contacts').then(res=>this.contacts = res)
+    // console.log('this.contacts:', this.contacts)
+    // await console.log(console.log('data:', data))
+    this.loading = false
   }
 })
+
+function request (url, method='GET', data=null) {
+  const headers = {}
+  let body
+
+  if (data) {
+    headers["Content-Type"] = 'application/json'
+    body = JSON.stringify(data)
+  }
+
+  try {
+    return fetch(url, {
+      method,
+      headers,
+      body,
+    }).then((res, rej)=> { return res.json() }).then((res)=> {
+      console.log('res:', res)
+      console.log(`Response is received:`, res)
+      return res
+      
+    })
+      
+      
+    
+  }
+  catch(e)
+  {
+    console.warn(`Error is appeared: ${e.message}`)
+    return false
+  }
+
+}
